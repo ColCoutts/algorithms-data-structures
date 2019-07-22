49a06041062a841608840bc47d7dfd5026d46a25
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-07-22 16:34:34 -0700

    stacks working

--- index.js
+++ index.js
@@ -277,3 +277,63 @@ console.log(bst.findMax());
 bst.remove(45);
 console.log(bst.findMax());
 console.log(bst.findMin());
+
+// simple stack using array methods
+let letters = [];
+let word = 'racecar';
+let rword = '';
+
+for(let i = 0; i < word.length; i++) {
+  letters.push(word[i]);
+}
+
+for(let i = 0; i < word.length; i++) {
+  rword += letters.pop();
+}
+
+if(rword === word) {
+  console.log(word + 'is a palindrome');
+} else {
+  console.log(word + 'is not a palindrome');
+}
+
+// Stack class not using array methods
+
+const Stack = function() {
+  this.count = 0;
+  this.storage = {};
+
+  this.push = function(value) {
+    this.storage[this.count] = value;
+    this.count++;
+  }
+
+  this.pop = function() {
+    if(this.count === 0) {
+      return undefined;
+    }
+    this.count--;
+    let result = this.storage[this.count];
+    delete this.storage[this.count];
+    return result;
+  }
+
+  this.size = function() {
+    return this.count;
+  }
+
+  this.peek = function() {
+    return this.storage[this.count - 1];
+  }
+}
+
+const myStack = new Stack();
+
+myStack.push(1);
+myStack.push(4);
+myStack.push(6);
+myStack.push(7);
+console.log(myStack.peek());
+myStack.pop();
+console.log(myStack.peek());
+console.log(myStack.size());
